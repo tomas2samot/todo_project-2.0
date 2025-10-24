0babc4cf248c2856099fb9b18cc8ee00e5fbae36
tomas2samot <mirga24siha@gmail.com>
2025-10-24 20:41:23 +0200

    Add files via upload

---
+++ todo_project/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/tasks/')),
+    path('tasks/', include(('task.urls', 'task'), namespace='task')),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/accounts/login/'), name='logout'),
+]
